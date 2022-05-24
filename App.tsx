8379817fcd19963ef0d8a06c25d62c47e64437e1
jandersonvb <jandersonpizziallyvilasboas@gmail.com>
2022-05-23 23:33:55 -0300

    feat: ensure custom fonts in the app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import AppLoading from 'expo-app-loading';
 
 import { ThemeProvider } from 'styled-components';
+
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
 
 import { Dashboard } from './src/screens/Dashboard/Dashboard';
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if (!fontsLoaded) { return <AppLoading /> }
   return (
     <ThemeProvider theme={theme}>
       <StatusBar backgroundColor="transparent" translucent barStyle="light-content" />
