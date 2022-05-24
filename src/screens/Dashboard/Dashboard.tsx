a9e890588bcbd29bb2fb97978f7d0df802638423
jandersonvb <jandersonpizziallyvilasboas@gmail.com>
2022-05-24 00:28:43 -0300

    feat: create component Header

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
-import { Container } from './Dashboard.styles';
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+} from './Dashboard.styles';
 
 const Dashboard: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/25486388?v=4' }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Janderson</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
+    </Container>
+  );
 };
 
 export { Dashboard };
